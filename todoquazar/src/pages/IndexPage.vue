fa7d0974cd2e8a77dc1e771d341212ec1fa27cbc
Vlad <vladanalyst@gmail.com>
2024-07-14 07:43:19 +0300

    Задание 11.1. выполнено

---
+++ todoquazar/src/pages/IndexPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://placebear.com/200/200">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <!-- <router-view /> -->
+      <q-btn color="primary" label="Primary" />
+      <div class="q-pa-md">
+    <q-video
+      src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+    />
+  </div>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://placebear.com/200/200">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
